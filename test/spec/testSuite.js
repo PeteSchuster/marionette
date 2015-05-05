679b6f8c55ad50258e32428a74e3119c4d1f3219
Schuster <Peter_Schuster@cable.comcast.com>
2015-05-05 08:31:27 -0400

    adds products collection

--- test/spec/testSuite.js
+++ test/spec/testSuite.js
@@ -3,7 +3,8 @@ define(function() {
 
 	/* return an array of specs to be run */
 	return {
-		specs: ['spec/exampleTest.js',
+		specs: ['spec/collections/products.js',
+		'spec/exampleTest.js',
 		'spec/models/product.js',
 		'spec/models/user.js',
 		'spec/views/item/product.js'
